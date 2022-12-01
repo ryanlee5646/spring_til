36356939df4c57e2e975a69da11fd135e34526c6
Gyujin Lee <ryanlee5646@gmail.com>
2022-12-02 00:38:19 +0900

    spring boot TIL MVC pattern

--- hello-spring/src/main/java/hello/hellospring/controller/HelloController.java
+++ hello-spring/src/main/java/hello/hellospring/controller/HelloController.java
@@ -4,6 +4,8 @@ package hello.hellospring.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HelloController {
@@ -13,4 +15,11 @@ public class HelloController {
         model.addAttribute("data", "hello!");
         return "hello";
     }
+    @GetMapping("hello-mvc")
+    // @RequestParam에서 required가 default로 true
+    public String helloMvc(@RequestParam(value = "name") String name, Model model){
+        model.addAttribute("name", name);
+        return "hello-template";
+    }
 }
+
