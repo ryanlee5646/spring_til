e3a0eba10096703001fb58e6ac1cb7808b08fe3f
Gyujin Lee <ryanlee5646@gmail.com>
2022-12-21 00:00:03 +0900

    spring til 회원가입 구현

---
+++ hello-spring/src/main/java/hello/hellospring/controller/MemberForm.java
@@ -0,0 +1,16 @@
+package hello.hellospring.controller;
+
+public class MemberForm {
+    private String name1;
+
+    public String getName() {
+        return name1;
+    }
+
+    public void setName(String name) {
+        this.name1 = name;
+    }
+
+
+
+}
