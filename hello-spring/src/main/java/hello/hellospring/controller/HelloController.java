dc6fb2c1fe4b4b50749bf178f5725b69f962c5a0
Gyujin Lee <ryanlee5646@gmail.com>
2023-07-31 23:01:01 +0900

    Test

--- hello-spring/src/main/java/hello/hellospring/controller/HelloController.java
+++ hello-spring/src/main/java/hello/hellospring/controller/HelloController.java
@@ -4,13 +4,12 @@ package hello.hellospring.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
-
+    // 테스트
     @GetMapping("hello")
     public String hello(Model model){
         model.addAttribute("data", "hello!");
